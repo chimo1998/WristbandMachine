8f4b623090774e9f23b09dc843b2f7abcdae8bda
WeiHsuan <whtsai1998@gmail.com>
2021-09-17 15:20:52 +0100

    Update

--- main.py
+++ main.py
@@ -6,20 +6,33 @@ import RPi.GPIO as gpio
 
 
 led = 25
-pir = 14
+red_led = 10
+green_led = 9
 
-bb_motor = Motor24v(720, 19, 16, 26)
-bs_motor = Motor24v(2880, 5, 20, 6)
-v12 = Motor12v(21, [4,17,27,22])
+hum1 = 23
+hum2 = 24
+
+band = 15
+
+bb_motor = Motor24v(720, 19, 26)
+bs_motor = Motor24v(2880, 5, 6)
+v12 = Motor12v([4,17,27,22])
 
 reader = Reader()
 printer = Printer()
 
 gpio.setmode(gpio.BCM)
 gpio.setup(led, gpio.OUT)
-gpio.setup(pir, gpio.OUT)
-gpio.output(pir, gpio.HIGH)
+gpio.setup(red_led, gpio.OUT)
+gpio.setup(green_led, gpio.OUT)
+gpio.setup(hum1, gpio.IN)
+gpio.setup(hum2, gpio.IN)
+gpio.setup(band, gpio.OUT)
+gpio.output(band, gpio.HIGH)
+gpio.setwarnings(False)
 
+gpio.output(red_led, gpio.HIGH)
+gpio.output(green_led, gpio.LOW)
 while(True):
     try:
         print("in main")
@@ -30,7 +43,6 @@ while(True):
             if data == "same":
                 data = None
                 continue
-            gpio.output(pir, gpio.HIGH)
 
         print(data)
         gpio.output(led, gpio.LOW)
@@ -52,9 +64,23 @@ while(True):
         # bb roll back
         bb_motor.spin(0, 0.8)
         # power up straper
-        gpio.output(pir, gpio.LOW)
+        gpio.output(green_led, gpio.HIGH)
+        gpio.output(red_led, gpio.LOW)
+        while (gpio.input(hum1) or gpio.input(hum2)):
+            time.sleep(0.1)
+        gpio.output(band, gpio.LOW)
+        time.sleep(1)
+        gpio.output(band, gpio.HIGH)
+        time.sleep(1)
+        while not ((gpio.input(hum1) and gpio.input(hum2))):
+            time.sleep(0.1)
+        gpio.output(green_led, gpio.LOW)
+        gpio.output(red_led, gpio.HIGH)
+        gpio.output(band, gpio.LOW)
+        time.sleep(1)
+        gpio.output(band, gpio.HIGH)
+        time.sleep(1)
 
-        time.sleep(0.1)
     except Exception:
         time.sleep(2)
         reader.previous_card_num = ""
--- motor.py
+++ motor.py
@@ -17,17 +17,12 @@ seq = [[1,0,0,1],
 #====================
 
 class Motor12v:
-    def __init__(self, btn, pins):
+    def __init__(self, pins):
         gpio.setmode(gpio.BCM)
-        gpio.setup(btn, gpio.IN)
         for pin in pins:
             gpio.setup(pin, gpio.OUT)
         self.pins = pins
-        self.btn = btn
         self.step = 0
-
-    def check_btn(self):
-        return gpio.input(self.btn)
 
     def spin(self, direction, turns):
         step = self.step
@@ -41,21 +36,15 @@ class Motor12v:
 
 
 class Motor24v:
-    def __init__(self, freq, pin, btn, direction):
+    def __init__(self, freq, pin, direction):
         gpio.setmode(gpio.BCM)
         gpio.setup(pin, gpio.OUT)
         motor = gpio.PWM(pin, freq)
         motor.start(0)
         self.motor = motor
 
-        gpio.setup(btn, gpio.IN)
-        self.btn = btn
-
         gpio.setup(direction, gpio.OUT)
         self.direction = direction
-
-    def check_btn(self):
-        return gpio.input(self.btn)
 
     def spin(self, direction, sec):
         gpio.output(self.direction, direction)
